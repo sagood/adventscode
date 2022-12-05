fc8339ad47664c7449789afbf35ba5f160ebeb77
Tian Yu <gasnus@gmail.com>
2022-12-05 13:01:33 +0100

    2015 Day 12

---
+++ 2015/12/p1.py
@@ -0,0 +1,25 @@
+import json
+
+
+def n(obj):
+    if type(obj) is dict:
+        return sum(map(n, obj.values()))
+
+    if type(obj) is list:
+        return sum(map(n, obj))
+
+    if type(obj) is int:
+        return obj
+
+    return 0
+
+
+def main():
+    with open('in.json', 'r') as f:
+        data = json.loads(f.read())
+        res = n(data)
+        print(res)
+
+
+if __name__ == '__main__':
+    main()
