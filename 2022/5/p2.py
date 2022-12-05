e65e649f641e22c7a6ea9b081c2e80520f594bad
Tian Yu <gasnus@gmail.com>
2022-12-05 08:20:35 +0100

    2022 Day 5

---
+++ 2022/5/p2.py
@@ -0,0 +1,23 @@
+import re
+
+
+def main():
+    with open('in.txt') as f:
+        inputs = f.read().split('\n')
+
+    s = [[] for _ in range(9)]
+    while not inputs[0].startswith(' 1'):
+        for i in range(9):
+            if inputs[0][1 + 4 * i] != ' ':
+                s[i].append(inputs[0][1 + 4 * i])
+        inputs.pop(0)
+
+    inputs = inputs[2:]
+    for c, f, t in [map(int, re.findall('\d+', l)) for l in inputs]:
+        s[t-1], s[f-1] = s[f-1][:c] + s[t-1], s[f-1][c:]
+
+    print(''.join(x[0] for x in s))
+
+
+if __name__ == '__main__':
+    main()
